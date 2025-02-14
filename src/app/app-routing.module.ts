bf7d39dcf6578259d7aa913fbc9f1b6951b6126d
arun <arun@ceroen.com>
2025-02-14 15:49:27 +0530

    routing about component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './component/about/about.component';
 
 const routes: Routes = [
   
   // {path: '', redirectTo:'/home' ,pathMatch:'full'},
   {path: '', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
 ];
 
 @NgModule({
